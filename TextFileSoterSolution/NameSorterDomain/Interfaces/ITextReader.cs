db7fe03cfdc1a31a51b57a175d54fcb28436fc9d
Tafadzwa.Stima <tafadzwa.stima@outlook.com>
2024-05-14 21:39:47 +0200

    implementation of the reading from text file functionality using streamreader

---
+++ TextFileSoterSolution/NameSorterDomain/Interfaces/ITextReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameSorterDomain.Interfaces
+{
+    public interface ITextReader
+    {
+        List<string> ReadFromFile(string filePath);
+    }
+}
---
+++ TextFileSoterSolution/NameSorterDomain/Services/TextReader.cs
@@ -0,0 +1,34 @@
+﻿using NameSorterDomain.Interfaces;
+
+namespace NameSorterDomain.Services
+{
+    public class TextReader : ITextReader
+    {
+        public List<string> ReadFromFile(string filePath)
+        {
+            List<string> names = new List<string>();
+
+            try
+            {
+               
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line;
+                    
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                       
+                        names.Add(line.Trim());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading from file: {ex.Message}");
+                return names;
+            }
+
+            return names;
+        }
+    }
+}
